ad2400fd550c9661981a9e9b2f034b65d4e83c98
Erick Zanardo <erickzanardoo@gmail.com>
2024-05-28 10:45:00 -0300

    .

--- bin/non_ascii_sandbox.dart
+++ bin/non_ascii_sandbox.dart
@@ -1,5 +1,6 @@
 import 'package:non_ascii_sandbox/non_ascii_sandbox.dart' as non_ascii_sandbox;
 
 void main(List<String> arguments) {
-  non_ascii_sandbox.run();
+  final usePackage = arguments.contains('--package');
+  non_ascii_sandbox.run(usePathPackage: usePackage);
 }
--- lib/non_ascii_sandbox.dart
+++ lib/non_ascii_sandbox.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 import 'package:path/path.dart' as path;
 
-void run() {
+void run({ required bool usePathPackage }) {
+  final pathFn = usePathPackage ? path.join : joinPath;
+
   final char = '木';
 
   final dir = Directory(
-    path.join(
+    pathFn(
       Directory.systemTemp.createTempSync().path,
       char,
     ),
@@ -13,8 +15,21 @@ void run() {
 
   print('Created directory: ${dir.path}');
 
-  final file = File(path.join(dir.path, 'file.txt'))..writeAsStringSync('Hello World');
+  final file = File(pathFn(dir.path, 'file.txt'))..writeAsStringSync('Hello World');
 
   print('Created file: ${file.path}');
   print('File content: ${file.readAsStringSync()}');
 }
+
+// So we don't use package just for this.
+String joinPath([String? part1, String? part2, String? part3, String? part4, String? part5]) {
+  final parts = <String>[];
+  if (part1 != null) parts.add(part1);
+  if (part2 != null) parts.add(part2);
+  if (part3 != null) parts.add(part3);
+  if (part4 != null) parts.add(part4);
+  if (part5 != null) parts.add(part5);
+
+  return parts.join(Platform.pathSeparator);
+}
+
